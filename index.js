881a9e99a8cd007ca36045b5f8d1575f1f85381f
FarxatDevelop <107210253+FarxatDevelop@users.noreply.github.com>
2024-08-01 15:53:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require("./core/index");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const express = require("express");
+const app = express()
 const { bot } = require("./core/bot");
 
 mongoose.connect(process.env.MONGO_DB).then(() => {
+  app.use(
+      await bot.createWebhook({
+        domain: "https://members-counter-bot.onrender.com",
+      })
+    );
   bot.launch();
 });
